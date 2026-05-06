89ff4a14c371fc91017a6d44f71619d7b033390f
Steve2370 <pougouebrice@gmail.com>
2026-05-06 12:20:12 -0400

    Feat: Logs

--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
     Route::get('/', [AdminController::class, 'index'])->name('index');
     Route::get('/logs', [AdminController::class, 'logs'])->name('logs');
     Route::get('/logs/export', [AdminController::class, 'exportLogs'])->name('logs.export');
-    Route::delete('/admin/users/{user}', [AdminController::class, 'supprimerUser'])->name('admin.users.supprimer');
+    Route::delete('/users/{user}', [AdminController::class, 'supprimerUser'])->name('users.supprimer');
 });
 
 Route::get('/privacy', fn() => view('privacy'))->name('privacy');
