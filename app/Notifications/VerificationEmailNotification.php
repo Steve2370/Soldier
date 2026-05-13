26475ff290c1113e54fdd09dc61f486ec291664d
Steve2370 <pougouebrice@gmail.com>
2026-05-13 09:50:00 -0400

    Feat: coffre familial automatique + partage auto membres

--- app/Notifications/VerificationEmailNotification.php
+++ app/Notifications/VerificationEmailNotification.php
@@ -1,18 +1,15 @@
 <?php
-
 namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\URL;
 
 class VerificationEmailNotification extends VerifyEmail
 {
     protected function buildMailMessage($url): MailMessage
     {
         return (new MailMessage)
+            ->mailer('resend')
             ->subject('Vérifiez votre adresse email — Soldier')
             ->view('emails.verification', ['url' => $url]);
     }
